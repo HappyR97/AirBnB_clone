417d14970a20092a4064540a69968506385fe26d
HappyR97 <ridaberkaoui1997@gmail.com>
2024-03-18 16:20:17 +0000

    Solve task 3

---
+++ models/base_model.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+
+This module defines all common attributes/methods for other classes
+
+"""
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """Base class"""
+
+    def __init__(self):
+        """Initializes values"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """String representation"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Updates updated_at with current time"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dict with all key/values of __dict__"""
+        dictionary = self.__dict__.copy()
+        dictionary['__class__'] = self.__class__.__name__
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        return dictionary
