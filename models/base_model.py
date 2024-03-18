2f7543fd35f788b4378e4149be6d380faa0dc339
HappyR97 <ridaberkaoui1997@gmail.com>
2024-03-18 17:02:58 +0000

    Solve task 4

--- models/base_model.py
+++ models/base_model.py
@@ -13,11 +13,21 @@ from datetime import datetime
 class BaseModel:
     """Base class"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializes values"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key in ['created_at', 'updated_at']:
+                    setattr(self, key, datetime.fromisoformat(value))
+                else:
+                    setattr(self, key, value)
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """String representation"""
